7a5c609caadca8cab6b8072718c9c0ffe4e8ba8d
mahun <mcintyre.mlm@gmail.com>
2021-07-01 22:09:12 -0400

    remove unused

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -7,7 +7,6 @@ import fleckTheme from '../../themes/fleckTheme'
 import Layout from '../../components/Layout'
 import FleckHeader from '../../components/FleckHeader'
 import FleckFooter from '../../components/FleckFooter'
-import FleckHero from '../../components/FleckHero'
 import Hero from '../../components/abstractions/Hero'
 
 const ContactPage = () => {
