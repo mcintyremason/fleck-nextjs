10f938657c8c417d1a09e1a27d6428a85a9233d9
Mason McIntyre <mcintyre.mlm@gmail.com>
2021-07-24 21:43:19 -0400

    Residential Page (#10)
    * residential txt
    
    * residential page alignment & hero min-height
    
    * roofers with integrity
    
    * residentail cta
    
    * finish residential

--- components/FleckCTA/index.tsx
+++ components/FleckCTA/index.tsx
@@ -1,43 +1,42 @@
 import styles from './index.module.css'
 import classNames from 'classnames'
 
-import React from 'react'
-import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined'
-import { Grid, GridSize, Typography } from '@material-ui/core'
+import React, { ReactNode } from 'react'
+import { Grid, GridSize } from '@material-ui/core'
 import { Breakpoint } from '@material-ui/core/styles/createBreakpoints'
 
 import CTA, { Color } from '../abstractions/CTA'
 
 type FleckCTAProps = {
-  // description: string
-  // imgurl: string
-  // title: string
   color: Color
   borderColor?: Color
-} & Partial<Record<Breakpoint, boolean | GridSize>>
+  children?: ReactNode
+  icon?: ReactNode
+} & Partial<Record<Breakpoint, boolean | GridSize>> &
+  React.HTMLAttributes<HTMLDivElement>
 
 const FleckCTA: React.FC<FleckCTAProps> = (props: FleckCTAProps) => {
-  const { color, borderColor, xs, sm, md, lg, xl } = props
+  const { children, className, color, borderColor, icon, xs, sm, md, lg, xl } = props
 
   return (
-    <Grid item xs={xs} sm={sm} md={md} lg={lg} xl={xl} className={styles['fleck-cta-container']}>
+    <Grid
+      item
+      xs={xs}
+      sm={sm}
+      md={md}
+      lg={lg}
+      xl={xl}
+      className={classNames(styles['fleck-cta-container'], className)}
+    >
       <CTA color={color} borderColor={borderColor} xs={12}>
-        <Grid container direction="column">
-          <Grid container justify="center">
-            <ChatOutlinedIcon fontSize="large" className={classNames(styles['fleck-cta-icon'])} />
-          </Grid>
-          <Grid
-            container
-            justify="center"
-            className={classNames(styles['fleck-cta-text'], styles['italic'])}
-          >
-            <Typography variant="body2" align="center">
-              Did the work as scheduled. Constantly stayed in touch. Answered all my questions.
-              Would recommend them!
-            </Typography>
-            <Typography variant="subtitle2" align="center" color="textPrimary">
-              - Virginia S., HomeAdvisor Review
-            </Typography>
+        <Grid container direction="column" className={styles['fleck-cta']}>
+          {icon && (
+            <Grid container justify="center" className={styles['fleck-cta-icon']}>
+              {icon}
+            </Grid>
+          )}
+          <Grid container justify="center" className={classNames(styles['fleck-cta-text'])}>
+            {children}
           </Grid>
         </Grid>
       </CTA>
